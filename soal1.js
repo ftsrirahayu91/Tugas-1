b79c0fff455de7b1211ea5e0fdebee972c52e1cb
ftsrirahayu91 <ftsrirahayu91@gmail.com>
2022-02-02 13:26:49 -0800

    tugas1

---
+++ soal1.js
@@ -0,0 +1,17 @@
+
+//no1
+function luas (alas, tinggi) {
+    console.log(0.5 * alas * tinggi);
+}
+luas(8,10)
+
+function luas (alas, tinggi) {
+    console.log(0.5 * alas * tinggi);
+}
+luas(5,7)
+
+function luas (alas, tinggi) {
+    console.log(0.5 * alas * tinggi);
+}
+luas(9,2)
+
---
+++ soal2.js
@@ -0,0 +1,12 @@
+//no2
+function lessThan(number1, number2) {
+    if(number1 + number2 <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(lessThan(22,15));
+console.log(lessThan(83,34));
+console.log(lessThan(3,77));
